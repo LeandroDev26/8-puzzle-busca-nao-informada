ed5132219b8f199955963b2f0544677a05d37624
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-13 12:23:32 -0300

    feat

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -173,7 +173,7 @@ void resolverBFS(array<int,9> inicio, array<int,9> objetivo)
         auto fim_tempo = chrono::high_resolution_clock::now();
         auto duracao = chrono::duration_cast<chrono::milliseconds>(fim_tempo - inicio_tempo);
         cout << "Busca esgotada nao existe nenhuma solucao possivel!o algoritmo vasculhou " << visitados.size() << " tabuleiros diferentes.\n\n";
-        cout<<"Quantidade de nos expandidos(visitados):"<< setw(8) << nodesExpandidos << "\n";
+        cout<< "Quantidade de nos expandidos(visitados):" << setw(8) << nodesExpandidos << "\n";
         cout << "Pico da fronteira: " << setw(6) << pico << "\n";
         cout << "Tempo:             " << setw(5) << duracao.count() << " ms\n";
     }
@@ -215,7 +215,7 @@ void resolverDFS(array<int,9> inicio, array<int,9> objetivo)
 
             cout << "Objetivo encontrado em " << atual->profundidade << " jogadas!\n\n";
             cout << "O algoritmo vasculhou " << visitados.size() << " tabuleiros diferentes.\n\n";
-            cout<<"Quantidade de nos expandidos(visitados):"<< setw(8) << nodesExpandidos << "\n";
+            cout<< "Quantidade de nos expandidos(visitados):" << setw(8) << nodesExpandidos << "\n";
             cout << "Pico da fronteira: " << setw(6) << pico << "\n";
             cout << "Tempo:             " << setw(5) << duracao.count() << " ms\n";
 
