b5a5e0100dd26da8ee82e52d3c757bf1f94e918b
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-14 22:56:37 -0300

    feat

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -289,7 +289,7 @@ int main()
     //caso2(medio)
     //array<int, 9> inicio = {1, 2, 3, 0, 4, 6, 7, 5, 8};
     //caso(complexo)
-    array<int, 9> inicio = {0, 1, 3, 4, 2, 5, 7, 8, 6};
+    //array<int, 9> inicio = {0, 1, 3, 4, 2, 5, 7, 8, 6};
     array<int, 9> objetivo = {1, 2, 3, 4, 5, 6, 7, 8, 0};
 
     resolverBFS(inicio, objetivo);
