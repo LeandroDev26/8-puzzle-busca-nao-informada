9dfb75edcefa9407f6d793b1ed3a943f2b9b658c
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-15 08:10:27 -0300

    feat

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -290,6 +290,11 @@ int main()
     //array<int, 9> inicio = {1, 2, 3, 0, 4, 6, 7, 5, 8};
     //caso(complexo)
     //array<int, 9> inicio = {0, 1, 3, 4, 2, 5, 7, 8, 6};
+
+    // entrada de tabuleiro dinamica
+    for(int i = 0 ; i < 9; i++){
+
+    }
     array<int, 9> objetivo = {1, 2, 3, 4, 5, 6, 7, 8, 0};
 
     resolverBFS(inicio, objetivo);
