aed61aa5a84e807dc9a551fdf51211de0ad9a8ee
Leandro Davi Borges Costa <261009786+LeandroDev26@users.noreply.github.com>
2026-09-15 08:17:28 -0300

    feat

--- Algorithms/main.cpp
+++ Algorithms/main.cpp
@@ -292,11 +292,10 @@ int main()
     //array<int, 9> inicio = {0, 1, 3, 4, 2, 5, 7, 8, 6};
 
     // entrada de tabuleiro dinamica
-    int x;
     array<int, 9> inicio;
     for(int i = 0 ; i < 9; i++){
      cout<<"Digite o "<< i << "o elmento do tabuleiro!"<<endl;
-
+     cin>> inicio[i];
 
     }
     array<int, 9> objetivo = {1, 2, 3, 4, 5, 6, 7, 8, 0};
